5ac9f2f436ad7b746e1cd2ecd633fdfb66330fe9
James Sabio <jamesjacobsabio232@gmail.com>
2025-07-20 18:40:04 +0800

    Finished first part of maze game

--- scripts/game.js
+++ scripts/game.js
@@ -1,33 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const roseContainer = document.querySelector('.rose-container');
-  const yesBtn = document.querySelector('.yes-btn');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const roseContainer = document.querySelector('.rose-container');
+//   const yesBtn = document.querySelector('.yes-btn');
 
-  function createRose() {
-    const rose = document.createElement('div');
-    rose.classList.add('rose');
+//   function createRose() {
+//     const rose = document.createElement('div');
+//     rose.classList.add('rose');
 
-    rose.style.left = `${Math.random() * 100}vw`;
+//     rose.style.left = `${Math.random() * 100}vw`;
 
-    const duration = Math.random() * 4 + 3;
-    rose.style.animationDuration = `${duration}s`;
+//     const duration = Math.random() * 4 + 3;
+//     rose.style.animationDuration = `${duration}s`;
 
-    const delay = Math.random() * 2;
-    rose.style.animationDelay = `${delay}s`;
+//     const delay = Math.random() * 2;
+//     rose.style.animationDelay = `${delay}s`;
 
-    roseContainer.appendChild(rose);
+//     roseContainer.appendChild(rose);
 
-    rose.addEventListener('animationend', () => {
-      rose.remove();
-    })
-  }
+//     rose.addEventListener('animationend', () => {
+//       rose.remove();
+//     })
+//   }
 
-  const roseInterval = setInterval(createRose, 2000);
+//   const roseInterval = setInterval(createRose, 1000);
 
   
-  yesBtn.addEventListener('click', () => {
-    clearInterval(roseInterval);
-    setTimeout(() => {
-      window.location.href = 'index.html'
-    }, 100);
-  });
-});
+//   yesBtn.addEventListener('click', () => {
+//     clearInterval(roseInterval);
+//     setTimeout(() => {
+//       window.location.href = 'index.html'
+//     }, 100);
+//   });
+// });
